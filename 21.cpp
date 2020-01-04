005e133e573cac14fb55ed5fde8cd8900cca707b
lovehhf <853885165@qq.com>
2020-01-04 20:43:14 +0800

    21.  合并两个有序链表 ,  链表

---
+++ 21.cpp
@@ -0,0 +1,32 @@
+/**
+ * 21. Merge Two Sorted Lists
+ * 
+ * 链表的简单语法题
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+        ListNode *dummy = new ListNode(-1);
+        ListNode *p = dummy;
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                p->next = l1;
+                l1 = l1->next;
+            } else {
+                p->next = l2;
+                l2 = l2->next;
+            }
+            p = p->next;
+        }
+        if (l1) p->next = l1;
+        if (l2) p->next = l2;
+        return dummy->next;
+
+    }
+};
