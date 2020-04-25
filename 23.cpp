8142cc150b36ce57eadbd0c7c9238a88302f40d1
huanghongfei <853885165@qq.com>
2020-04-26 00:36:47 +0800

    [UPDATE] 23. 合并K个排序链表, 基于分治优化了时间复杂度

--- 23.cpp
+++ 23.cpp
@@ -1,10 +1,13 @@
 
 
 
-/** 23. Merge k Sorted Lists
- * https://leetcode.com/problems/merge-k-sorted-lists/
+/** 23. 合并K个排序链表
+ * https://leetcode-cn.com/problems/merge-k-sorted-lists/
+ * 顺序合并, 时间复杂度 O(K^2n), k 为数组大小, n 为最长链表长度
  * 先合并2条链表, 再用和结果和其他链表合并(reduce)
- * 时间复杂度高了, 待优化
+ *
+ * 基于分支思想优化：
+ * 类似归并排序, 每条链表只和相邻的链表合并
  *
  * Definition for singly-linked list.
  * struct ListNode {
@@ -13,6 +16,7 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
 class Solution {
 public:
     ListNode* merge2Lists(ListNode *p, ListNode *q) {
@@ -32,15 +36,17 @@ public:
         if (q) head->next = q;
         return dummy->next;
     }
-    
+
+    // 分治, 每条链表只和与它相邻的链表合并, 时间复杂度O(log(n)), n为nlog(n), n为数组长度
+    ListNode* merge(vector<ListNode*>& lists, int l, int r) {
+        if (l >= r) return lists[l];
+        int mid = (l + r) >> 1;
+        ListNode *left = merge(lists, l, mid);
+        ListNode *right = merge(lists, mid + 1, r);
+        return merge2Lists(left, right);
+    }
+
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if(lists.empty()) return NULL;
-        if(lists.size() == 1) return lists[0];
-        
-        ListNode *res = merge2Lists(lists[0], lists[1]);
-        for(int i = 2; i < lists.size(); i ++) {
-            res = merge2Lists(res, lists[i]);
-        }
-        return res;
+        return lists.size() == 0 ? nullptr : merge(lists, 0, lists.size() - 1);
     }
 };
